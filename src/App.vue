7232f0199f89a2978310ec157e02e29f5aa77634
huawei <vhuawei@sina.com>
2024-05-17 08:32:03 +0800

    uodate

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,24 @@
  * @version: 
  * @Author: 华伟
  * @Date: 2024-03-07 19:30:32
- * @LastEditTime: 2024-05-14 15:20:53
+ * @LastEditTime: 2024-05-16 14:30:00
 -->
 <template>
   <div id="app">
-    <GanttChart :default-date="'day'"/>
+    <!-- <GanttChart :default-date="'day'"/> -->
+    <ganttElastic></ganttElastic>
   </div>
 </template>
 
 <script>
 import GanttChart from './components/gantt-chart/index.vue'
+import ganttElastic from './components/gantt-elastic/index.vue'
 
 export default {
   name: 'App',
   components: {
-    GanttChart
+    GanttChart,
+    ganttElastic
   }
 }
 </script>
