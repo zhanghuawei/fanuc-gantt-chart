72b80f4fb14c79d4fb26312c6c7a9d4b59d85d56
huawei <vhuawei@sina.com>
2024-05-22 15:06:59 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,27 @@
  * @version: 
  * @Author: 华伟
  * @Date: 2024-03-07 19:30:32
- * @LastEditTime: 2024-05-16 14:30:00
+ * @LastEditTime: 2024-05-20 09:30:39
 -->
 <template>
   <div id="app">
     <!-- <GanttChart :default-date="'day'"/> -->
-    <ganttElastic></ganttElastic>
+    <!-- <ganttElastic></ganttElastic> -->
+    <vueGanttastic></vueGanttastic>
   </div>
 </template>
 
 <script>
 import GanttChart from './components/gantt-chart/index.vue'
 import ganttElastic from './components/gantt-elastic/index.vue'
+import vueGanttastic from './components/vueGanttastic/index.vue'
 
 export default {
   name: 'App',
   components: {
     GanttChart,
-    ganttElastic
+    ganttElastic,
+    vueGanttastic
   }
 }
 </script>
---
+++ src/components/vueGanttastic/index.vue
@@ -0,0 +1,55 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: 华伟
+ * @Date: 2024-05-20 09:28:53
+ * @LastEditTime: 2024-05-20 09:29:09
+-->
+<template>
+  <g-gantt-chart :chart-start="myChartStart" :chart-end="myChartEnd">
+    <g-gantt-row v-for="row in rows" :key="row.label" :label="row.label" :bars="row.bars" bar-start="myStart"
+      bar-end="myEnd" />
+  </g-gantt-chart>
+</template>
+<script>
+
+import { GGanttChart, GGanttRow } from 'vue-ganttastic'
+
+export default {
+  components: {
+    GGanttChart,
+    GGanttRow
+  },
+
+  data() {
+    return {
+      myChartStart: "2020-03-01 00:00",
+      myChartEnd: "2020-03-03 00:00",
+      rows: [
+        {
+          label: "My row #1",
+          bars: [
+            {
+              myStart: "2020-03-01 12:10",
+              myEnd: "2020-03-01 16:35"
+            }
+          ]
+        },
+        {
+          label: "My row #2",
+          bars: [
+            {
+              myStart: "2020-03-02 01:00",
+              myEnd: "2020-03-02 12:00"
+            },
+            {
+              myStart: "2020-03-02 13:00",
+              myEnd: "2020-03-02 22:00"
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
